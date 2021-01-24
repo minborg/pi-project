0c433318f9641ca73decd1ac18c47778adb25c98
Per Minborg <minborg@speedment.com>
2021-01-24 10:36:23 +0100

    Improve servo

--- src/main/java/minborg/piproject/Servo.java
+++ src/main/java/minborg/piproject/Servo.java
@@ -1,16 +1,33 @@
 package minborg.piproject;
 
 import com.pi4j.component.servo.ServoProvider;
+import com.pi4j.component.servo.impl.MaestroServoDriver;
 import com.pi4j.component.servo.impl.MaestroServoProvider;
-
-import java.io.IOException;
 
 public final class Servo {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ServoProvider servoProvider = new MaestroServoProvider();
 
-        System.out.println(servoProvider.getDefinedServoPins());
+        System.out.println("Supported pins:" + servoProvider.getDefinedServoPins());
+
+        MaestroServoDriver servo0 = (MaestroServoDriver) servoProvider.getServoDriver(servoProvider.getDefinedServoPins().get(0));
+
+        System.out.println("servo0 = " + servo0);
+
+        long start = System.currentTimeMillis();
+
+        int min = servo0.getMinValue();
+        int max = servo0.getMaxValue();
+        servo0.setAcceleration(30);
+
+        while (System.currentTimeMillis() - start < 120000) { // 2 minutes
+            servo0.setServoPulseWidth(min);
+            Thread.sleep(1500);
+            servo0.setServoPulseWidth(max);
+            Thread.sleep(1500);
+        }
+        System.out.println("Exiting MaestroServoExample");
 
     }
 
